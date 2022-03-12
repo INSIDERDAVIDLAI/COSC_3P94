54d8fd2d2d4bf3a0e9ad9a5aedc52e69227030f4
Jiayang Lai <insiderdavidlai@gmail.com>
2022-03-12 01:04:23 -0500

    simple pic switch complete

---
+++ Stage 2/js/script.js
@@ -0,0 +1,16 @@
+var picArr=new Array("int1.jpg","int2.jpg","int3.jpg","int4.jpg");
+var index=0;
+function next(){
+    index++;
+    if(index==picArr.length){
+        index=0;
+    }
+    document.getElementById("img").src=picArr[index];
+}
+function pre(){
+    index--;
+    if(index<0){
+        index=picArr.length-1;
+    }
+    document.getElementById("img").src=picArr[index];
+}
